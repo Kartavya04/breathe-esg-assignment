b6b8bd11215607926ef93956ceb2549b7f84c0cb
Kartavya04 <151157953+Kartavya04@users.noreply.github.com>
2026-05-25 04:37:14 +0530

    Fix line endings for linux

--- backend/build.sh
+++ backend/build.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Upgrade pip, then force install setuptools globally in the venv
-python -m pip install --upgrade pip
-pip install setuptools wheel
+pip install --upgrade pip
 pip install -r requirements.txt
 
-# Run migrations and collectstatic
 python manage.py collectstatic --no-input
 python manage.py migrate
